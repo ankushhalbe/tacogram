9b508a26bed6f0ba3b9c5c467135d2e767564299
Ankush  Halbe <ankush.halbe@kellogg.northwestern.edu>
2024-02-28 05:55:52 +0000

    cc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     @user["last_name"] = params["last_name"]
     @user["email"] = params["email"]
     # TODO: encrypt user's password "at rest"
+    
     @user["password"] = params["password"]
     @user.save
     redirect_to "/posts"
