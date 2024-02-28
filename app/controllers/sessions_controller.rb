7090dea41b0370925fd8f32899ade1466ffcbdf0
Ankush  Halbe <ankush.halbe@kellogg.northwestern.edu>
2024-02-28 06:23:37 +0000

    CC

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,21 @@ class SessionsController < ApplicationController
   
   def create
     # TODO: authenticate user
+    @user = User.find_by({ "email" => params["email"] })
+
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome, #{@user["first_name"]}."
+        ADD COOKIE
+        redirect_to "/posts"        
+      else
+        flash["notice"] ="Nope"
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/login"
   end
+
 end
